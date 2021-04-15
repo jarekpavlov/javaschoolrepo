a8396d533f57b89b2ce7bcf3958a1dbf3022e545
Jarek <jarekpavlov@gmail.com>
2021-04-15 17:34:25 +0300

    Creating JPA connection (with persistance.xml file)

--- src/main/java/com/jschool/controllers/HelloController.java
+++ src/main/java/com/jschool/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.jschool.controllers;
 
+import com.jschool.domain.Client;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
@@ -8,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -16,26 +20,22 @@ public class HelloController {
 
     @RequestMapping(value = "",method = RequestMethod.GET)
     public String hello(){
-        DriverManagerDataSource dataSource=new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/mydb?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("pit_bull");
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("ClientUnit");
+        EntityManager entityManager = factory.createEntityManager();
+        entityManager.getTransaction().begin();
 
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        Client client = new Client();
+        client.setAddress("Russia");
+        client.setDateofbirth("12.12.1999");
+        client.setName("John");
+        client.setSurename("Johnson");
+        client.setPhone("+7///");
 
-        String sql ="SELECT* from clients WHERE id=1";
-        ResultSetExtractor<String> resultSetExtractor = new ResultSetExtractor<String>() {
-            @Override
-            public String extractData(ResultSet resultSet) throws SQLException, DataAccessException {
-                if (resultSet.next()){
-                    String name = resultSet.getString("name");
-                    return name;
-                }
-                return null;
-            }
-        };
-        System.out.println( jdbcTemplate.query(sql,resultSetExtractor));
+        entityManager.persist(client);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        factory.close();
         return "hello";
     }
 }
---
+++ src/main/java/com/jschool/domain/Client.java
@@ -0,0 +1,66 @@
+package com.jschool.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "clients")//If the name of the table in db is different
+public class Client {
+
+    private Long id;
+    private String name;
+    private String surename;
+    private String phone;
+    private String address;
+    private String dateofbirth;
+
+    //@Column should be used if the name of column in DB does not match to class field
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurename() {
+        return surename;
+    }
+
+    public void setSurename(String surename) {
+        this.surename = surename;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDateofbirth() {
+        return dateofbirth;
+    }
+
+    public void setDateofbirth(String dateofbirth) {
+        this.dateofbirth = dateofbirth;
+    }
+}
